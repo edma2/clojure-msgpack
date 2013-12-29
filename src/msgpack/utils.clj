7d6f0046d98d672d5d13bec218e5938649c15ab2
Eugene Ma <ema@twitter.com>
2013-12-28 22:40:36 -0500

    Add some docs for utils

--- src/msgpack/utils.clj
+++ src/msgpack/utils.clj
@@ -1,3 +1,6 @@
+;; Utilities for specifying bytes as if they were hex literals.
+;; Ex. (byte 0xc0) will produce a cast error. (byte-literal 0xc0) will perform
+;; the required math needed to fit the bit-pattern 0xc0 into a Java byte.
 (ns msgpack.utils)
 
 (defn byte-literal [x]
