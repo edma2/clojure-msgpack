a1901c89f2cc824d397a861b822df73523296a7d
Eugene Ma <ema@twitter.com>
2013-12-29 02:55:36 -0500

    signed/unsigned ints up to 32 bits

--- src/msgpack/serializer.clj
+++ src/msgpack/serializer.clj
@@ -11,9 +11,20 @@
   (if bool (byte-literals [0xc3])
     (byte-literals [0xc2])))
 
+(defn- int-bytes
+  [x n]
+  (if (= 0 n) []
+  (conj (int-bytes (bit-shift-right x 8) (dec n))
+        (byte-literal (bit-and 0xff x)))))
+
 (defmethod serialize Long
   [x]
   (cond
     (<= 0 x 127) (byte-literals [x])
     (<= -32 x -1) (byte-literals [x])
-    (<= 0 x 0xff) (byte-literals [0xcc x])))
+    (<= 0 x 0xff) (byte-literals [0xcc x])
+    (<= 0 x 0xffff) (cons (byte-literal 0xcd) (int-bytes x 2))
+    (<= 0 x 0xffffffff) (cons (byte-literal 0xce) (int-bytes x 4))
+    (<= -0x80 x 0x7f) (byte-literals [0xd0 x])
+    (<= -0x8000 x 0x7fff) (cons (byte-literal 0xd1) (int-bytes x 2))
+    (<= -0x80000000 x 0x7fffffff) (cons (byte-literal 0xd2) (int-bytes x 4))))
--- test/msgpack/serializer_test.clj
+++ test/msgpack/serializer_test.clj
@@ -14,12 +14,35 @@
 
 (deftest int-test
   (testing "ints"
+    ; 7-bit positive int
     (is (= (byte-literals [0x00]) (serialize 0)))
     (is (= (byte-literals [0x10]) (serialize 0x10)))
     (is (= (byte-literals [0x7f]) (serialize 0x7f)))
+    ; 5-bit negative int
     (is (= (byte-literals [0xff]) (serialize -1)))
     (is (= (byte-literals [0xf0]) (serialize -16)))
     (is (= (byte-literals [0xe0]) (serialize -32)))
+    ; 8-bit uint
     (is (= (byte-literals [0xcc 0x80]) (serialize 0x80)))
     (is (= (byte-literals [0xcc 0xf0]) (serialize 0xf0)))
-    (is (= (byte-literals [0xcc 0xff]) (serialize 0xff)))))
+    (is (= (byte-literals [0xcc 0xff]) (serialize 0xff)))
+    ; 16-bit uint
+    (is (= (byte-literals [0xcd 0x01 0x00]) (serialize 0x100)))
+    (is (= (byte-literals [0xcd 0x20 0x00]) (serialize 0x2000)))
+    (is (= (byte-literals [0xcd 0xff 0xff]) (serialize 0xffff)))
+    ; 32-bit uint
+    (is (= (byte-literals [0xce 0x00 0x01 0x00 0x00]) (serialize 0x10000)))
+    (is (= (byte-literals [0xce 0x00 0x20 0x00 0x00]) (serialize 0x200000)))
+    (is (= (byte-literals [0xce 0xff 0xff 0xff 0xff]) (serialize 0xffffffff)))
+    ; 8-bit int
+    (is (= (byte-literals [0xd0 0xdf]) (serialize -33)))
+    (is (= (byte-literals [0xd0 0x9c]) (serialize -100)))
+    (is (= (byte-literals [0xd0 0x80]) (serialize -128)))
+    ; 16-bit int
+    (is (= (byte-literals [0xd1 0xff 0x7f]) (serialize -129)))
+    (is (= (byte-literals [0xd1 0xf8 0x30]) (serialize -2000)))
+    (is (= (byte-literals [0xd1 0x80 0x00]) (serialize -32768)))
+    ; 32-bit int
+    (is (= (byte-literals [0xd2 0xff 0xff 0x7f 0xff]) (serialize -32769)))
+    (is (= (byte-literals [0xd2 0xc4 0x65 0x36 0x00]) (serialize -1000000000)))
+    (is (= (byte-literals [0xd2 0x80 0x00 0x00 0x00]) (serialize -2147483648)))))
