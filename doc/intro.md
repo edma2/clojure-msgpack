4ff4ff3c50671a9b3b2ae94579ec8790406db13d
Eugene Ma <ema@twitter.com>
2013-12-28 21:15:56 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-msgpack
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_msgpack/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-msgpack.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-msgpack.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
