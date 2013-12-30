8f818208fefb9cca3a2a1a1118e331278d19568e
Eugene Ma <ema@twitter.com>
2013-12-30 14:36:50 -0500

    Minor cleanup

--- src/msgpack/serializer.clj
+++ src/msgpack/serializer.clj
@@ -23,11 +23,10 @@
     (<= 0 x 0xffff) (with-header 0xcd (get-short-bytes x))
     (<= 0 x 0xffffffff) (with-header 0xce (get-int-bytes x))
     (<= 0 x 0x7fffffffffffffff) (with-header 0xcf (get-long-bytes x))
-    (<= -0x80 x 0x7f) (with-header 0xd0 (get-byte-bytes x))
-    (<= -0x8000 x 0x7fff) (with-header 0xd1 (get-short-bytes x))
-    (<= -0x80000000 x 0x7fffffff) (with-header 0xd2 (get-int-bytes x))
-    (<= -0x8000000000000000 x 0x7fffffffffffffff)
-      (with-header 0xd3 (get-long-bytes x))))
+    (<= -0x80 x -1) (with-header 0xd0 (get-byte-bytes x))
+    (<= -0x8000 x -1) (with-header 0xd1 (get-short-bytes x))
+    (<= -0x80000000 x -1) (with-header 0xd2 (get-int-bytes x))
+    (<= -0x8000000000000000 x -1) (with-header 0xd3 (get-long-bytes x))))
 
 ; Long can handle up to 2^63-1 integers. MessagePack max is 2^64-1.
 ; Clojure coerces values greater than 2^63-1 to BigInts.
@@ -38,7 +37,7 @@
     ; In case they use small values with explicit BigInt type, e.g. 314N
     (serialize (.longValue x))))
 
-; Cast everything else to Long
+; Cast all other integral types to Long
 (defmethod serialize Integer [x] (serialize (long x)))
 (defmethod serialize Short [x] (serialize (long x)))
 (defmethod serialize Byte [x] (serialize (long x)))
