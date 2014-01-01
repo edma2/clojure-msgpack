6fbfa8aa2cd7c37cc4f6c2f2f8fa0de937684ccb
Eugene Ma <ema@twitter.com>
2014-01-01 13:18:16 -0800

    More renamings

--- src/msgpack/utils.clj
+++ src/msgpack/utils.clj
@@ -2,26 +2,26 @@
   (:import java.io.ByteArrayOutputStream
            java.io.DataOutputStream))
 
-(defn ubyte [x]
-  (byte (if (> x 0x7f) (- x 0x100) x)))
+(defn ubyte [n]
+  (byte (if (> n 0x7f) (- n 0x100) n)))
 
-(defn ubytes [xs]
-  (map ubyte xs))
+(defn ubytes [seq]
+  (map ubyte seq))
 
-(defn ubyte-array [xs]
-  (byte-array (ubytes xs)))
+(defn ubyte-array [seq]
+  (byte-array (ubytes seq)))
 
 (defn- get-bytes
   "Convert a Java primitive to its byte representation."
-  [write x]
+  [write n]
   (let [output-stream (ByteArrayOutputStream.)
         data-output (DataOutputStream. output-stream)]
-    (write data-output x)
+    (write data-output n)
     (.toByteArray output-stream)))
 
-(defn get-byte-bytes [x] (get-bytes #(.writeByte %1 %2) x))
-(defn get-short-bytes [x] (get-bytes #(.writeShort %1 %2) x))
-(defn get-int-bytes [x] (get-bytes #(.writeInt %1 %2) x))
-(defn get-long-bytes [x] (get-bytes #(.writeLong %1 %2) x))
+(defn get-byte-bytes [n] (get-bytes #(.writeByte %1 %2) n))
+(defn get-short-bytes [n] (get-bytes #(.writeShort %1 %2) n))
+(defn get-int-bytes [n] (get-bytes #(.writeInt %1 %2) n))
+(defn get-long-bytes [n] (get-bytes #(.writeLong %1 %2) n))
 (defn get-float-bytes [n] (get-bytes #(.writeFloat %1 %2) n))
 (defn get-double-bytes [n] (get-bytes #(.writeDouble %1 %2) n))
