9bb31bd10e7d0888c3ba7419ed7a31943311f52e
Eugene Ma <ema@twitter.com>
2014-01-01 00:46:50 -0500

    Add docstring to top level serialize

--- src/msgpack/core.clj
+++ src/msgpack/core.clj
@@ -1,5 +1,7 @@
 (ns msgpack.core
   (:require [msgpack.serializer :as serializer]))
 
-(defn serialize [thing]
+(defn serialize
+  "Serialize an object as a sequence of bytes in MessagePack format."
+  [thing]
   (serializer/serialize thing))
