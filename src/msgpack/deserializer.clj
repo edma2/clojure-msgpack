ebec653bd7c543297b90167469e9bb94b2315af6
Eugene Ma <ema@twitter.com>
2014-01-02 15:20:10 -0800

    Add deserialization code for positive-fixint.

--- src/msgpack/deserializer.clj
+++ src/msgpack/deserializer.clj
@@ -4,42 +4,47 @@
 
 (defn- as-unsigned [byte] (bit-and 0xff byte))
 
+(defn- deserialize-positive-fixint [bytes]
+  (bit-and 0x7f (first bytes)))
+
+(defn- ??? [] (throw (UnsupportedOperationException.)))
+
 (defn deserialize [bytes]
   (let [first-byte (-> bytes first as-unsigned)]
     (cond
-      (<= 0x00 first-byte 0x7f) nil
-      (<= 0x80 first-byte 0x8f) nil
-      (<= 0x90 first-byte 0x9f) nil
-      (<= 0xa0 first-byte 0xbf) nil
+      (<= 0x00 first-byte 0x7f) (deserialize-positive-fixint bytes)
+      (<= 0x80 first-byte 0x8f) (???)
+      (<= 0x90 first-byte 0x9f) (???)
+      (<= 0xa0 first-byte 0xbf) (???)
       (= first-byte 0xc0) nil
       (= first-byte 0xc2) false
       (= first-byte 0xc3) true
-      (= first-byte 0xc4) nil
-      (= first-byte 0xc5) nil
-      (= first-byte 0xc6) nil
-      (= first-byte 0xc7) nil
-      (= first-byte 0xc8) nil
-      (= first-byte 0xc9) nil
-      (= first-byte 0xca) nil
-      (= first-byte 0xcb) nil
-      (= first-byte 0xcc) nil
-      (= first-byte 0xcd) nil
-      (= first-byte 0xce) nil
-      (= first-byte 0xcf) nil
-      (= first-byte 0xd0) nil
-      (= first-byte 0xd1) nil
-      (= first-byte 0xd2) nil
-      (= first-byte 0xd3) nil
-      (= first-byte 0xd4) nil
-      (= first-byte 0xd5) nil
-      (= first-byte 0xd6) nil
-      (= first-byte 0xd7) nil
-      (= first-byte 0xd8) nil
-      (= first-byte 0xd9) nil
-      (= first-byte 0xda) nil
-      (= first-byte 0xdb) nil
-      (= first-byte 0xdc) nil
-      (= first-byte 0xdd) nil
-      (= first-byte 0xde) nil
-      (= first-byte 0xdf) nil
-      (<= 0xe0 first-byte 0xff) nil)))
+      (= first-byte 0xc4) (???)
+      (= first-byte 0xc5) (???)
+      (= first-byte 0xc6) (???)
+      (= first-byte 0xc7) (???)
+      (= first-byte 0xc8) (???)
+      (= first-byte 0xc9) (???)
+      (= first-byte 0xca) (???)
+      (= first-byte 0xcb) (???)
+      (= first-byte 0xcc) (???)
+      (= first-byte 0xcd) (???)
+      (= first-byte 0xce) (???)
+      (= first-byte 0xcf) (???)
+      (= first-byte 0xd0) (???)
+      (= first-byte 0xd1) (???)
+      (= first-byte 0xd2) (???)
+      (= first-byte 0xd3) (???)
+      (= first-byte 0xd4) (???)
+      (= first-byte 0xd5) (???)
+      (= first-byte 0xd6) (???)
+      (= first-byte 0xd7) (???)
+      (= first-byte 0xd8) (???)
+      (= first-byte 0xd9) (???)
+      (= first-byte 0xda) (???)
+      (= first-byte 0xdb) (???)
+      (= first-byte 0xdc) (???)
+      (= first-byte 0xdd) (???)
+      (= first-byte 0xde) (???)
+      (= first-byte 0xdf) (???)
+      (<= 0xe0 first-byte 0xff) (???))))
