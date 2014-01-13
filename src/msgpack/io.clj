6b37a0062e3005ad4ce9c8aeed7c62dcd4107086
Eugene Ma <edma12123@gmail.com>
2014-01-12 19:08:45 -0800

    Typos

--- src/msgpack/io.clj
+++ src/msgpack/io.clj
@@ -58,7 +58,8 @@
 (defn next-float [stream] (.readFloat stream))
 (defn next-double [stream] (.readDouble stream))
 (defn next-bytes [n stream]
-  (assert (.read stream (byte-array n) n))
-  bytes)
+  (let [bytes (byte-array n)]
+    (assert (.read stream bytes))
+    bytes))
 
-(defn next-string [n stream] (.String (next-bytes n stream)))
+(defn next-string [n stream] (String. (next-bytes n stream)))
