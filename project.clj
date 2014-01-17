49dbf465366f1bf342444ed3ea8e097d1de0f7df
Eugene Ma <edma12123@gmail.com>
2014-01-16 23:55:30 -0800

    MIT license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-msgpack "0.1.0-SNAPSHOT"
   :description "Pure Clojure implementation of MessagePack spec."
   :url "https://github.com/edma2/clojure-msgpack"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
