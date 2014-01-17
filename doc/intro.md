5d3358f52ed468f3e909038046dd307d8349d10e
Eugene Ma <edma12123@gmail.com>
2014-01-16 23:30:21 -0800

    Prep for deploy

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-msgpack
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-msgpack "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Pure Clojure implementation of MessagePack spec."
+  :url "https://github.com/edma2/clojure-msgpack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
