908add92e4c310d40d467787e047c28799ff3da6
Eugene Ma <ema@twitter.com>
2015-03-19 01:50:41 -0700

    add :scm

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :scm {:name "git"
+        :url "https://github.com/edma2/clojure-msgpack"})
