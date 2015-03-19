09b66aebe4f1c46186bb9be8fdd4acc2b037a162
Eugene Ma <ema@twitter.com>
2015-03-19 01:35:57 -0700

    update project.clj dependencies and description

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject clojure-msgpack "0.1.0-SNAPSHOT"
-  :description "Pure Clojure implementation of MessagePack spec."
+  :description "A lightweight Clojure implementation of the MessagePack spec."
   :url "https://github.com/edma2/clojure-msgpack"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [reiddraper/simple-check "0.5.3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :global-vars {*warn-on-reflection* true})
