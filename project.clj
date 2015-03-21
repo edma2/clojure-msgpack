36f88ec10bc3faeb258b0f115c86e74317c08203
Eugene Ma <ema@twitter.com>
2015-03-21 02:32:21 -0700

    Add clojure.test.check test.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/edma2/clojure-msgpack"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/test.check "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :scm {:name "git"
         :url "https://github.com/edma2/clojure-msgpack"})
---
+++ test/msgpack/core_check.clj
@@ -0,0 +1,32 @@
+(ns msgpack.core-check
+  (:require [msgpack.core :refer [pack unpack]]
+            [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
+
+(defmulti normalize class)
+(defmethod normalize :default [v] v)
+
+(defmethod normalize clojure.lang.Ratio [r]
+  (let [d (double r)]
+    (if (<= d Float/MAX_VALUE) (float d) d)))
+
+(defmethod normalize (Class/forName "[B") [bytes] (seq bytes))
+(defmethod normalize Character [c] (str c))
+(defmethod normalize clojure.lang.Keyword [k] (name k))
+(defmethod normalize clojure.lang.Symbol [s] (name s))
+
+(defmethod normalize clojure.lang.Sequential [seq]
+  (map normalize seq))
+
+(defmethod normalize clojure.lang.IPersistentSet [set]
+  (normalize (vec set)))
+
+(defmethod normalize clojure.lang.IPersistentMap [m]
+  (into {} (for [[k v] m] [(normalize k) (normalize v)])))
+
+(defspec unpack-will-undo-pack 20
+  (prop/for-all [v (gen/vector gen/any)]
+                (= (unpack (pack v))
+                   (normalize v))))
