8ab6a50ed590385c08455a7b01dd4f7966602c9a
Eugene Ma <ema@twitter.com>
2016-04-04 23:33:10 -0700

    test for explicit types instead of 'any'

--- test/msgpack/core_check.clj
+++ test/msgpack/core_check.clj
@@ -1,17 +1,33 @@
 (ns msgpack.core-check
   (:require [msgpack.core :as msg]
-            [msgpack.macros :refer [extend-msgpack]]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
-; gen/any can return UUIDs
-(extend-msgpack
- java.util.UUID
- 42
- [id] (msg/pack (str id))
- [bytes] (java.util.UUID/fromString (msg/unpack bytes)))
+; NaN is never equal to itself
+(defn- not-nan [x]
+  (not (Double/isNaN x)))
 
-(defspec round-trip 40
-  (prop/for-all [v gen/any]
-                (= (msg/unpack (msg/pack v)) v)))
+(defn- pack-and-unpack [x] (msg/unpack (msg/pack x)))
+
+(defspec ints-round-trip 100
+  (prop/for-all [x gen/int]
+                (= (pack-and-unpack x) x)))
+
+(defspec floats-round-trip 100
+  (prop/for-all [x (gen/such-that not-nan gen/double)]
+                (= (pack-and-unpack x) x)))
+
+(defspec bytes-round-trip 100
+  (prop/for-all [x gen/bytes]
+                (let [bytes (pack-and-unpack x)]
+                  (and (instance? (Class/forName "[B") bytes)
+                       (= (seq bytes) (seq x))))))
+
+(defspec strings-round-trip 100
+  (prop/for-all [x gen/string]
+                (= (pack-and-unpack x) x)))
+
+(defspec vectors-round-trip 100
+  (prop/for-all [x (gen/vector (gen/map gen/int gen/string))]
+                (= (pack-and-unpack x) x)))
